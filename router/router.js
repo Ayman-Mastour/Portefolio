9c03e7f9f7a903338b0f5c2690bc4613697b5dda
Ayman-Mastour <140393642+Ayman-Mastour@users.noreply.github.com>
2024-03-11 17:32:19 +0100

    Initial commit

---
+++ router/router.js
@@ -0,0 +1,18 @@
+import express from "express";
+const router = express.Router()
+
+
+router.get("/", (req, res) =>{
+    res.render('home')
+})
+
+router.get("/projet-1", (req, res) =>{
+    res.render("projet-1")
+})
+
+router.get("/send", (req, res) => {
+    res.render("send")
+})
+
+
+export default router;
---
+++ server.js
@@ -0,0 +1,60 @@
+import  express from "express";
+import router from "./router/router.js";
+import nodemailer from "nodemailer"
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 7016;
+const hostname = "localhost";
+
+export const BASE_URL = `http://${hostname}:${port}`
+
+
+app.use(express.static("public"));
+app.use("/img", express.static("img"));
+
+
+app.set("views","./views");
+app.set("view engine", "ejs");
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use("/", router);
+
+// middleware pour analyser le corps de la requête
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//route pour gérer le formulaire POST
+app.post('/send', (req, res) => {
+    const { name, email, message} = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'aymen.mst91@gmail.com',
+            pass: 'ldrx qziu lfae vfcn'
+        }
+    });
+
+    const mailOptions = {
+        from: 'aymen.mst91@gmail.com',
+        to: 'aymen.mst91@gmail.com',
+        subject: 'message portefolio',
+        text: `Nom: ${name}\nEmail: ${email}\nMessage: ${message}`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email envoyé:' + info.response);
+        res.redirect('/send')
+    })
+})
+
+app.listen(port, () => {
+    console.log(`tu est connecté ${BASE_URL}`);
+})
